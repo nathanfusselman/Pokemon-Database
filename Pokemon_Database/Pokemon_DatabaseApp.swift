3ace2ca6886b2847187282bc8fb4d9b27f9d7f37
Nathan Fusselman <44579712+nathanfusselman@users.noreply.github.com>
2021-04-05 12:53:05 -0500

    Initial Commit

---
+++ Pokemon_Database/Pokemon_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_DatabaseApp.swift
+//  Pokemon_Database
+//
+//  Created by Nathan Fusselman on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
